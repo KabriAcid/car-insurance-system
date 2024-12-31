8de41934a0c35955ac3faa9453906e532260bd5f
Abdullahi Kabri <kabriacid01@gmail.com>
2024-12-31 22:25:06 +0100

    commit

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Insurance System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi nihil laboriosam quibusdam ullam ab esse delectus, dignissimos, eos alias odio laborum explicabo? Recusandae aliquam dolorum reprehenderit odio nostrum vel itaque.</p>
+</body>
+</html>
